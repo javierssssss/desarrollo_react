025af8f5dc31b9ed62070605883f73668805fb7a
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-06-24 21:24:01 -0500

    Builded

---
+++ build/precache-manifest.c44806fc05451dc3366e09527547301c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1678a0e6579c936c5be73326e93453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7419fb74eba7418a9397",
+    "url": "/static/css/main.09b22928.chunk.css"
+  },
+  {
+    "revision": "31a5c32b33dc253a10e9",
+    "url": "/static/js/2.9ec1f125.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.9ec1f125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7419fb74eba7418a9397",
+    "url": "/static/js/main.0d8e8de4.chunk.js"
+  },
+  {
+    "revision": "5a5bb4fe9f1a8936736d",
+    "url": "/static/js/runtime-main.e4f762a2.js"
+  },
+  {
+    "revision": "59559e2ca05f84ca6b35d87b6c0cdade",
+    "url": "/static/media/circulo.59559e2c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
